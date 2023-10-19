938ede79d19589ee0113a52e83ff235436a9cdb1
pdcm11 <pdcm11@gmail.com>
2023-10-19 20:26:45 +0100

    imagemPronta

--- app/src/main/java/com/example/pdmaula1/MainActivity.kt
+++ app/src/main/java/com/example/pdmaula1/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.pdmaula1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pdmaula1.ui.theme.PDMAula1Theme
 
@@ -28,10 +32,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+    Row{
+        Column {
+            Text(text = "Pedro Marques")
+            Text(text = "19/10/2023")
+        }
+        Image(painter = painterResource(id = R.drawable.skydive1) , contentDescription = "logotipo"
+        )
+    }
 }
 
 @Preview(showBackground = true)
